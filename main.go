42418f42c24985c7e4cedab9efbbb7434327e9e3
equk <equk@users.noreply.github.com>
2018-08-31 15:11:37 +0100

    added security related headers using chi middleware

--- main.go
+++ main.go
@@ -25,6 +25,13 @@ func main() {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
 
+	// Set security headers using middleware
+	r.Use(middleware.SetHeader("X-DNS-Prefetch-Control", "off"))
+	r.Use(middleware.SetHeader("X-Frame-Options", "SAMEORIGIN"))
+	r.Use(middleware.SetHeader("Strict-Transport-Security", "max-age=15552000; includeSubDomains"))
+	r.Use(middleware.SetHeader("X-Download-Options", "noopen"))
+	r.Use(middleware.SetHeader("X-Content-Type-Options", "nosniff"))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		response := []Article{
 			{
