b9c35bec361385b89fc85807e943a8ee646a2364
equk <equk@users.noreply.github.com>
2018-08-31 13:34:43 +0100

    added Article struct & now returning a generic json response

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
+
+type Article struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
 
 func main() {
 	r := chi.NewRouter()
@@ -20,7 +26,13 @@ func main() {
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		response := []Article{
+			{
+				Title: "API Listening",
+				Body:  "API Server ver: 0.1",
+			},
+		}
+		render.JSON(w, r, response)
 	})
 	fmt.Println("Starting server on port 3000")
 	http.ListenAndServe(":3000", r)
