b928909559e58d22adbfd3215a9ea120ae42ddeb
equk <equk@users.noreply.github.com>
2018-08-31 12:46:56 +0100

    added chi middleware & added message to show server is listening

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
 	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
+	fmt.Println("Starting server on port 3000")
 	http.ListenAndServe(":3000", r)
 }
